69fab51e5e583f5b036f113577736e802c014b61
MarioFelipe14 <mafebape140498@gmail.com>
2024-10-16 23:14:07 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
-console.log('olá mundo!');
+AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const tempoAteEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor (tempoAteEvento /diaEmMs);
+    const horasAteOEvento = Math.floor ((tempoAteEvento % diaEmMs) / horaEmMs );
+    const minutosAteOEvento = Math.floor((tempoAteEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor ((tempoAteEvento % minutoEmMs) / 1000 );
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m  ${segundosAteOEvento}s`;
+
+    if(tempoAteEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = `Evento expirado`;
+    }
+},1000);
